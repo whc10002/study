7e8cf97452bd1c076feb32dccb342227c1d328b2
whc10002 <whc10002@sina.com>
2017-10-19 03:14:28 +0000

    use find to replace find_first_of

--- my_code/cplusplus/string_substr/string_substr.cpp
+++ my_code/cplusplus/string_substr/string_substr.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <string.h>
 
 using namespace std;
 
@@ -7,19 +8,28 @@ string check_and_correct_invalid_url(const string & url)
 {
 	string result = url;
 	size_t first_slash_pos,first_qmark_pos;
-	first_qmark_pos = url.find_first_of('?');
-	first_slash_pos = url.find_first_of('/',8);
+	first_qmark_pos = url.find('?');
 	if (first_qmark_pos == string::npos)
 	{	
 		cout << "return_1" << endl;
         	return result;
 	}
+	size_t start_pos = url.find("://");
+	if (start_pos == string::npos)
+	{
+		start_pos = 1;
+	}
+	else
+	{
+		start_pos += strlen(("://"));
+	}
+	first_slash_pos = url.find('/',start_pos);
 	if (first_slash_pos != string::npos && first_slash_pos < first_qmark_pos)
 	{
 		cout << "return_2" << endl;
         	return result;
 	}
-	cout << "modify" << endl;
+	cout << first_qmark_pos << ":" << first_slash_pos << "\t"<< "modify" << endl;
 	result = url.substr(0,first_qmark_pos) + "/" + url.substr(first_qmark_pos,url.length() - first_qmark_pos);
 	return result;
 }
@@ -34,4 +44,11 @@ int main(int argc, char *argv[])
 	
 	url = "http://www.baidu.com/findle?a=2";
 	cout << check_and_correct_invalid_url(url) << endl;
+	
+	url = "www.baidu.com?a=2";
+	cout << check_and_correct_invalid_url(url) << endl;
+	
+	url = "www.baidu.com/?a=2";
+	cout << check_and_correct_invalid_url(url) << endl;
+	
 }
