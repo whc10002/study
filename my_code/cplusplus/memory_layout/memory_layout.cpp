bc45a6cf14b229ff7752b59a0503207d8dd5912b
whc10002 <whc10002@sina.com>
2018-03-02 14:51:53 +0000

    add multiple inheritance memory layout

--- my_code/cplusplus/memory_layout/memory_layout.cpp
+++ my_code/cplusplus/memory_layout/memory_layout.cpp
@@ -13,6 +13,16 @@ class base
 		int bval;
 };
 
+class base_1
+{
+	public:
+		base_1(int bv_1):bval_1(bv_1){};
+		virtual void base_1_f(){cout<<"base_1::f()" << endl;}
+		virtual void base_1_g(){cout<<"base_1::g()" << endl;}
+	private:
+		int bval_1;
+};
+
 class derived: public base
 {
 	public:
@@ -22,12 +32,23 @@ class derived: public base
 		int dval;
 };
 
+class derived_multi: public base, public base_1
+{
+	public:
+		derived_multi(int bv1, int bv2):base(bv1), base_1(bv2){};
+		virtual void derived_multi_h(){cout << "derived_multi::h()" << endl;}
+};
+
 typedef void (*FUN)();
 
 int main(int argc,char** argv)
 {
-	base b(100);
 	FUN fun;
+
+	//base:vptr+bval
+	//	vptr->[0]->base::base_f()
+	//	vptr->[1]->base::base_g()
+	base b(100);
 	FUN **pvtab = (FUN**)&b;
 	cout<<"[0]:base->vptr:" << pvtab <<endl;
 
@@ -41,19 +62,55 @@ int main(int argc,char** argv)
 	int * pval = (int*)(pvtab + 1);
 	cout<<"[1]:bval "<<pval[0]<<endl;
 	
+	//derived:vptr+bval+dval
+	//	vptr->[0]->base::base_f()
+	//	vptr->[1]->base::base_g()
+	//	vptr->[2]->derived::derived_h()
 	derived d(10, 100);
 	pvtab = (FUN**)&d;
 	cout<<"[0]:derived->vptr "<<endl;
-	for(int i=0; i<2; i++)
+	for(int i=0; i<3; i++)
 	{
 		fun = (FUN)pvtab[0][i];
 		cout<<" "<<i<<" ";
 		fun();
 	}
 
-	pval = (int*)(pvtab+1);
 	cout<<"[1]:bval "<<pval[0]<<endl;
 	cout<<"[2]:dval "<<pval[1]<<endl;
+
+	//derived_multi:vptr1+bval+vptr2+bval_1
+	//	base:vptr1+bval
+	//		vptr1->[0]->base::base_f()
+	//		vptr1->[1]->base::base_g()
+	//		vptr1->[2]->derived_multi::derived_multi_h()
+	//		bval
+	//	base_1:vptr2+bval_1
+	//		vptr2->[0]->base_1::base_1_f()
+	//		vptr2->[1]->base_1::base_1_g()
+	//		bval_1
+	derived_multi d_m(234,456);
+	pvtab = (FUN**)&d_m;
+	cout<<"[0]:derived_multi->vptr1(base->vptr) " <<endl;
+	for (int i=0; i<3; i++)
+	{
+		fun = (FUN)pvtab[0][i];
+		cout<<" "<<i<<" ";
+		fun();
+	}
+	pval = (int*)(pvtab+1);
+	cout<<"[1]:base::bval "<<pval[0]<<endl;
+
+	pvtab = pvtab + 2;
+	cout<<"[2]:derived_multi->vptr2(base_1->vptr) " <<endl;
+	for (int i=0; i<2; i++)
+	{
+		fun = (FUN)pvtab[0][i];
+		cout<<" "<<i<<" ";
+		fun();
+	}
+	pval = (int*)(pvtab+1);
+	cout<<"[3]:base_1::bval_1 "<<pval[0]<<endl;
 
 	return 0;
 }
