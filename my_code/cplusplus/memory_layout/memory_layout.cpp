68fa24e8e00c1453e9172cad4ea5b8b68f0d41f1
whc10002 <whc10002@sina.com>
2018-03-02 07:38:24 +0000

    add c++ memory layout

--- my_code/cplusplus/memory_layout/memory_layout.cpp
+++ my_code/cplusplus/memory_layout/memory_layout.cpp
@@ -7,8 +7,8 @@ class base
 {
 	public:
 		base(int bv):bval(bv){};
-		virtual void base_f(){cout<<"base2::f()"<<endl;}
-		virtual void base_g(){cout<<"base2::g()"<<endl;}
+		virtual void base_f(){cout<<"base::f()" << endl;}
+		virtual void base_g(){cout<<"base::g()" << endl;}
 	private:
 		int bval;
 };
@@ -17,7 +17,6 @@ class derived: public base
 {
 	public:
 		derived(int bv1, int dv):base(bv1), dval(dv){};
-		virtual void base_f(){cout<<"derived::f()"<<endl;}
 		virtual void derived_h(){cout<<"derived::h()"<<endl;}
 	private:
 		int dval;
@@ -27,31 +26,34 @@ typedef void (*FUN)();
 
 int main(int argc,char** argv)
 {
-	base b(10);
+	base b(100);
 	FUN fun;
-	int **pvtab = (int**)&b;
-	cout<<"[0]:base->vptr"<<endl;
+	FUN **pvtab = (FUN**)&b;
+	cout<<"[0]:base->vptr:" << pvtab <<endl;
+
 	for(int i=0; i<2; i++)
 	{
 		fun = (FUN)pvtab[0][i];
 		cout<<" "<<i<<" ";
 		fun();
 	}
-	cout<<" 2 "<<pvtab[0][2]<<endl;
+
+	
 	cout<<"[1]:bval "<<(long)pvtab[1]<<endl;
 	
-	/*derived d(10, 100);
-	pvtab = (int**)&d;
+	derived d(10, 100);
+	pvtab = (FUN**)&d;
 	cout<<"[0]:derived->vptr "<<endl;
-	for(int i=0; i<3; i++)
+	for(int i=0; i<2; i++)
 	{
 		fun = (FUN)pvtab[0][i];
 		cout<<" "<<i<<" ";
 		fun();
 	}
-	cout<<" 3 "<<pvtab[0][3]<<endl;
-	cout<<"[1]:bval "<<(int)pvtab[1]<<endl;
-	cout<<"[2]:dval "<<(int)pvtab[2]<<endl;
-	*/
+
+	int * pval = (int*)(pvtab+1);
+	cout<<"[1]:bval "<<pval[0]<<endl;
+	cout<<"[2]:dval "<<pval[1]<<endl;
+
 	return 0;
 }
