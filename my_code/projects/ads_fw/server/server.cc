37574afccaa44baa92d93aeb5f3d497bbd725612
Hongwei Cao <hwcao@freewheel.tv>
2018-07-19 02:46:04 +0000

    add code

--- my_code/projects/ads_fw/server/server.cc
+++ my_code/projects/ads_fw/server/server.cc
@@ -1,3 +1,19 @@
+// -*- C++ -*-
+// =============================================================================
+/**
+ *	Copyright (c) Freewheel, 2007-2009. All rights reserved.
+ *
+ *	@file
+ *
+ *	@autho
+ *
+ * 	@brief
+ *
+ *	Revision History:
+ *			2018/05/03	Created
+ */
+// =============================================================================
+#include <getopt.h>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -6,6 +22,21 @@ using namespace std;
 
 int main(int argc, char**argv)
 {
+	typedef std::shared_ptr<ads::Sig_Action> Sig_Action_Ptr;
+	
+	int retval = 0;
+	bool run_as_server = false;
+
+	//instantiate singletons
+	(void)Ads_Log_Service::instance();
+	(void)Ads_Pusher_Service::instance();
+
+	evthread_use_pthreads();
+	
+	curl_global_init(CURL_GLOBAL_ALL);
+	init_openssl_locks();
+
+	CrcGenerateTable();
 	cout << "server::start()" << endl;
 	cout << "server::end()" << endl;
 	return 0;
