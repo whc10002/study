5e5b01c7846be90cca6842aed59fc15deeb5a2e0
hwcao <hwcao@freewheel.com>
2020-12-22 10:37:46 +0000

    update

--- my_code/cplusplus/have_common_element/build_bench.sh
+++ my_code/cplusplus/have_common_element/build_bench.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-g++ bench_mark.cpp -std=c++11 -isystem /usr/local/include/benchmark \
-  -Lbenchmark/build/src -lbenchmark -lpthread -o benchmark
+g++ bench_mark.cpp -std=c++11  -O3 -isystem /usr/local/include/benchmark \
+  -lbenchmark -lpthread -o benchmark
